53e056d04e27b112a9e2d1c92901e7c690dd1347
mjohns <michael.r.johns@gmail.com>
2018-11-21 15:47:57 -0500

    transform

---
+++ src/util/transform.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace kb {
+
+class Transform {
+ public:
+  double x = 0;
+  double y = 0;
+  double z = 0;
+
+  double rx = 0;
+  double ry = 0;
+  double rz = 0;
+}:
+
+}  // namespace kb
---
+++ src/util/transform_test.cc
@@ -0,0 +1,18 @@
+#include <glm/glm.hpp>
+#include <iostream>
+
+#include "util/transform.h"
+
+using std::cout;
+using std::endl;
+
+using namespace kb;
+
+int main() {
+  Transform t;
+  t.z = 2;
+  t.rx = 90;
+
+  glm::vec3 point = t.ApplyToPoint(glm::vec3(0, 0, 1));
+  cout << "x: " << point.x << " y: " << point.y << " z: " << point.z << endl;
+}

63be10836c71474d090a60baa7c4aabb5df886c5
mjohns <michael.r.johns@gmail.com>
2018-11-24 13:23:36 -0500

    transform

---
+++ src/util/transform.cc
@@ -0,0 +1,36 @@
+#include "util/transform.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+
+#include "util/scad.h"
+
+namespace kb {
+
+glm::vec3 Transform::Apply(const glm::vec3& p) const {
+  glm::mat4 transform(1.0f);
+  transform = glm::rotate(transform, glm::radians(rx), glm::vec3(1, 0, 0));
+  transform = glm::rotate(transform, glm::radians(ry), glm::vec3(0, 1, 0));
+  transform = glm::rotate(transform, glm::radians(rz), glm::vec3(0, 0, 1));
+  transform = glm::translate(transform, translation());
+
+  glm::vec4 transformed = transform * glm::vec4(p.x, p.y, p.z, 1);
+  return glm::vec3(transformed.x, transformed.y, transformed.z);
+}
+
+Shape ApplyTransforms(Shape shape, const std::vector<Transform>& transforms) {
+  for (auto rit = transforms.rbegin(); rit != transforms.rend(); ++rit) {
+    shape = rit->Apply(shape);
+  }
+  return shape;
+}
+
+glm::vec3 ApplyTransforms(glm::vec3 point, const std::vector<Transform>& transforms) {
+  for (auto rit = transforms.rbegin(); rit != transforms.rend(); ++rit) {
+    point = rit->Apply(point);
+  }
+  return point;
+}
+
+}  // namespace kb
--- src/util/transform.h
+++ src/util/transform.h
@@ -1,70 +1,51 @@
 #pragma once
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
 
 #include "util/scad.h"
 
 namespace kb {
 
-glm::vec3 Rotate(const glm::vec3& p, double deg, double x, double y, double z) {
-  glm::mat4 rotation(1.0f);
-  rotation = glm::rotate(rotation, glm::radians(deg), glm::vec3(x, y, z));
-  glm::vec4 rotated = rotation * glm::vec4(p.x, p.y, p.z, 1);
-  return glm::vec3(rotated.x, rotated.y, rotated.z);
-}
-
-gmtl::Point3d RotateX(const glm::vec3& p, double deg) {
-  return Rotate(p, deg, 1, 0, 0);
-}
-
-gmtl::Point3d RotateY(const glm::vec3& p, double deg) {
-  return Rotate(p, deg, 0, 1, 0);
-}
-
-gmtl::Point3d RotateZ(const glm::vec3& p, double deg) {
-  return Rotate(p, deg, 0, 0, 1);
-}
-
-struct Frame {
+struct Transform {
  public:
-  double x = 0;
-  double y = 0;
-  double z = 0;
+  float x = 0;
+  float y = 0;
+  float z = 0;
 
   // Rotation in degrees
-  double rx = 0;
-  double ry = 0;
-  double rz = 0;
+  float rx = 0;
+  float ry = 0;
+  float rz = 0;
 
-  Frame(double px, double py, double pz) {
+  Transform() {
+  }
+
+  Transform(double px, double py, double pz) {
     x = px;
     y = py;
     z = pz;
   }
 
-  glm::vec3 origin() const {
+  Transform(const glm::vec3& t) {
+    x = t.x;
+    y = t.y;
+    z = t.z;
+  }
+
+  glm::vec3 translation() const {
     return glm::vec3(x, y, z);
   }
 
-  Shape ApplyToShape(const Shape& in) {
-    return in.RotateZ(rz).RotateX(rx).RotateY(ry).Translate(x, y, z);
+  Shape Apply(const Shape& shape) const {
+    return shape.RotateZ(rz).RotateX(rx).RotateY(ry).Translate(x, y, z);
   }
 
-  glm::vec3 ApplyToPoint(const glm::vec3& p) {
-    glm::mat4 rotation(1.0f);
-    rotation = glm::rotate(rotation, glm::radians(rx), glm::vec3(1, 0, 0));
-    rotation = glm::rotate(rotation, glm::radians(ry), glm::vec3(0, 1, 0));
-    rotation = glm::rotate(rotation, glm::radians(rz), glm::vec3(0, 0, 1));
-
-    glm::vec4 rotated4 = rotation * glm::vec4(p.x, p.y, p.z, 1);
-    glm::vec3 rotated(rotated4.x, rotated4.y, rotated4.z);
-    return rotated + glm::vec3(x, y, z);
-  }
-
-  glm::vec3 ApplyToPoint() {
-    return ApplyToPoint(glm::vec3(0, 0, 0));
-  }
+  glm::vec3 Apply(const glm::vec3& p) const;
 };
+
+Shape ApplyTransforms(Shape shape, const std::vector<Transform>& transforms);
+
+glm::vec3 ApplyTransforms(glm::vec3 point, const std::vector<Transform>& transforms);
 
 }  // namespace kb

7341a2c6808a970e60713249bcbd0b8f20dd7586
Michael Johns <1633607+mjohns@users.noreply.github.com>
2019-03-09 07:18:26 -0500

    f

--- src/keyboards/keyboard.cc
+++ src/keyboards/keyboard.cc
@@ -1,3 +1,4 @@
+#include <functional>
 #include <vector>
 
 #include "util/key.h"
@@ -7,9 +8,9 @@
 using namespace kb;
 
 const bool kIncludeConnectors = true;
-const bool kUseCapsuleConnector = true;
+const bool kUseCapsuleConnector = false;
 const bool kShowManuform = false;
-const bool kAddCaps = false;
+const bool kAddCaps = true;
 
 Shape Connector() {
   return kUseCapsuleConnector ? GetCapsuleConnector() : GetPostConnector();
@@ -25,10 +26,17 @@ Shape Tri(const TransformList& t1,
 int main() {
   std::vector<Key*> keys;
 
+  double extra_width = 4;
+
   double radius = 60;
   double rotation = 17;
   Key top_template;
   top_template.local_transforms.TranslateZ(-1 * radius).RotateX(rotation).TranslateZ(radius);
+
+  auto configure_key = [&keys](Key* k, std::function<void(Key & k)> fn) {
+    fn(*k);
+    keys.push_back(k);
+  };
 
   Key bottom_template;
   bottom_template.local_transforms.TranslateZ(-1 * radius)
@@ -37,220 +45,272 @@ int main() {
 
   // Middle Finger - d
   Key m2{-3, 3, 28.5};
-  m2.t().ry = -15;
-  m2.AddTransform();
-  m2.t().x = -.3;
-  m2.t().z = 1;
-  m2.extra_width_left = 1;
-  m2.extra_width_right = 1;
-  keys.push_back(&m2);
+  configure_key(&m2, [&](Key& k) {
+    k.t().ry = -15;
+    k.AddTransform();
+    k.t().x = -.3;
+    k.t().z = 1;
+    k.extra_width_left = 1;
+    k.extra_width_right = 1;
+  });
 
   Key t2 = top_template;
-  t2.parent = &m2;
-  t2.extra_width_left = 1;
-  t2.extra_width_right = 1;
-  keys.push_back(&t2);
+  configure_key(&t2, [&](Key& k) {
+    k.parent = &m2;
+    k.extra_width_left = 1;
+    k.extra_width_right = 1;
+  });
 
   Key tt2 = top_template;
-  tt2.parent = &t2;
-  tt2.extra_height_top = 2;
-  keys.push_back(&tt2);
+  configure_key(&tt2, [&](Key& k) {
+    k.parent = &t2;
+    k.extra_height_top = 2;
+  });
 
   Key b2 = bottom_template;
-  b2.parent = &m2;
-  b2.extra_width_left = 1;
-  b2.extra_width_right = 1;
-  keys.push_back(&b2);
+  configure_key(&b2, [&](Key& k) {
+    k.parent = &m2;
+    k.extra_width_left = 1;
+    k.extra_width_right = 1;
+  });
 
   Key bb2 = bottom_template;
-  bb2.parent = &b2;
-  bb2.extra_width_right = 2;
-  bb2.extra_height_bottom = 2;
-  keys.push_back(&bb2);
+  configure_key(&bb2, [&](Key& k) {
+    k.parent = &b2;
+    k.extra_width_right = 2;
+    k.extra_height_bottom = 2;
+  });
 
   // Index Finger - f
   Key m1{19.5, -2.7, 5.1};
-  m1.parent = &m2;
-  m1.t().ry = -5;
-  m1.AddTransform();
-  m1.t().x = .2;
-  m1.t().z = .2;
-  m1.t().y = -.2;
-  m1.extra_z = 2;
-  keys.push_back(&m1);
+  configure_key(&m1, [&](Key& k) {
+    k.parent = &m2;
+    k.t().ry = -5;
+    k.AddTransform();
+    k.t().x = .2;
+    k.t().z = .2;
+    k.t().y = -.2;
+    k.extra_z = 2;
+  });
 
   Key t1 = top_template;
-  t1.parent = &m1;
-  t1.extra_z = 2;
-  keys.push_back(&t1);
+  configure_key(&t1, [&](Key& k) {
+    k.parent = &m1;
+    k.extra_z = 2;
+  });
 
   Key tt1 = top_template;
-  tt1.parent = &t1;
-  tt1.extra_z = 3;
-  tt1.extra_height_top = 2;
-  keys.push_back(&tt1);
+  configure_key(&tt1, [&](Key& k) {
+    k.parent = &t1;
+    k.extra_z = 3;
+    k.extra_height_top = 2;
+  });
 
   Key b1 = bottom_template;
-  b1.parent = &m1;
-  b1.extra_z = 2;
-  b1.extra_height_bottom = 5;
-  keys.push_back(&b1);
+  configure_key(&b1, [&](Key& k) {
+    k.parent = &m1;
+    k.extra_z = 2;
+    k.extra_height_bottom = 5;
+  });
 
   // Innermost Column - g
   Key m0{19.5, -1.0, 0.5};
-  m0.parent = &m1;
-  m0.t().ry = -5;
-  m0.AddTransform();
-  m0.t().y = 1;
-  m0.t().z = .3;
-  m0.t().x = .2;
-  m0.extra_width_right = 2;
-  keys.push_back(&m0);
+  configure_key(&m0, [&](Key& k) {
+    k.parent = &m1;
+    k.t().ry = -5;
+    k.AddTransform();
+    k.t().y = 1;
+    k.t().z = .3;
+    k.t().x = .2;
+    k.extra_width_right = extra_width;
+  });
 
   Key t0 = top_template;
-  t0.parent = &m0;
-  t0.extra_width_right = 2;
-  keys.push_back(&t0);
+  configure_key(&t0, [&](Key& k) {
+    k.parent = &m0;
+    k.extra_width_right = extra_width;
+  });
 
   Key tt0 = top_template;
-  tt0.parent = &t0;
-  tt0.extra_width_right = 2;
-  tt0.extra_height_top = 2;
-  keys.push_back(&tt0);
+  configure_key(&tt0, [&](Key& k) {
+    k.parent = &t0;
+    k.extra_width_right = extra_width;
+    k.extra_height_top = 2;
+  });
 
   Key b0 = bottom_template;
-  b0.parent = &m0;
-  b0.extra_z = 2;
-  b0.extra_width_right = 2;
-  keys.push_back(&b0);
+  configure_key(&b0, [&](Key& k) {
+    k.parent = &m0;
+    k.extra_z = 2;
+    k.extra_width_right = extra_width;
+  });
 
   // Ring Finger - s
   Key m3(-20, -3, 5.1);
-  m3.parent = &m2;
-  m3.t().ry = 5;
-  m3.AddTransform();
-  m3.t().x = .2;
-  m3.t().z = .28;
-  m3.extra_z = 2;
-  keys.push_back(&m3);
+  configure_key(&m3, [&](Key& k) {
+    k.parent = &m2;
+    k.t().ry = 5;
+    k.AddTransform();
+    k.t().x = .2;
+    k.t().z = .28;
+    k.extra_z = 2;
+  });
 
   Key t3 = top_template;
-  t3.parent = &m3;
-  t3.extra_z = 2;
-  keys.push_back(&t3);
+  configure_key(&t3, [&](Key& k) {
+    k.parent = &m3;
+    k.extra_z = 2;
+  });
 
   Key tt3 = top_template;
-  tt3.parent = &t3;
-  tt3.extra_z = 3;
-  tt3.extra_height_top = 2;
-  keys.push_back(&tt3);
+  configure_key(&tt3, [&](Key& k) {
+    k.parent = &t3;
+    k.extra_z = 3;
+    k.extra_height_top = 2;
+  });
 
   Key b3 = bottom_template;
-  b3.parent = &m3;
-  b3.extra_z = 2;
-  keys.push_back(&b3);
+  configure_key(&b3, [&](Key& k) {
+    k.parent = &m3;
+    k.extra_z = 2;
+  });
 
   Key bb3 = bottom_template;
-  bb3.parent = &b3;
-  bb3.extra_height_bottom = 2;
-  keys.push_back(&bb3);
+  configure_key(&bb3, [&](Key& k) {
+    k.parent = &b3;
+    k.extra_height_bottom = 2;
+  });
 
   // Pinky Finger - a
   Key m4{-20.2, -11.5, 6.3};
-  m4.t().y -= 2;
-  m4.parent = &m3;
-  m4.t().ry = 5;
-  m4.AddTransform();
-  m4.t().z = .2;
-  m4.t().y = -.45;
-  m4.extra_z = 2;
-  keys.push_back(&m4);
+  configure_key(&m4, [&](Key& k) {
+    k.t().y -= 2;
+    k.parent = &m3;
+    k.t().ry = 5;
+    k.AddTransform();
+    k.t().z = .2;
+    k.t().y = -.45;
+    k.extra_z = 2;
+  });
 
   Key t4 = top_template;
-  t4.parent = &m4;
-  t4.extra_z = 2;
-  keys.push_back(&t4);
+  configure_key(&t4, [&](Key& k) {
+    k.parent = &m4;
+    k.extra_z = 2;
+  });
 
   Key tt4 = top_template;
-  tt4.parent = &t4;
-  tt4.extra_z = 2;
-  tt4.extra_height_top = 4;
-  keys.push_back(&tt4);
+  configure_key(&tt4, [&](Key& k) {
+    k.parent = &t4;
+    k.extra_z = 2;
+    k.extra_height_top = 4;
+  });
 
   Key b4 = bottom_template;
-  b4.parent = &m4;
-  b4.extra_z = 2;
-  b4.extra_height_bottom = 4;
-  keys.push_back(&b4);
+  configure_key(&b4, [&](Key& k) {
+    k.parent = &m4;
+    k.extra_z = 2;
+    k.extra_height_bottom = 4;
+  });
 
   // Shift Column
   Key m5(-19.8, 0, 0.8);
-  m5.parent = &m4;
-  m5.t().ry = 5;
-  m5.extra_z = 2;
-  m5.extra_width_left = 2;
-  keys.push_back(&m5);
+  configure_key(&m5, [&](Key& k) {
+    k.parent = &m4;
+    k.t().ry = 5;
+    k.extra_z = 2;
+    k.extra_width_left = extra_width;
+  });
 
   Key t5 = top_template;
-  t5.parent = &m5;
-  t5.extra_z = 2;
-  t5.extra_width_left = 2;
-  keys.push_back(&t5);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &m5;
+    k.extra_z = 2;
+    k.extra_width_left = extra_width;
+  });
 
   Key tt5 = top_template;
-  tt5.parent = &t5;
-  tt5.extra_z = 2;
-  tt5.extra_height_top = 4;
-  tt5.extra_width_left = 2;
-  keys.push_back(&tt5);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &t5;
+    k.extra_z = 2;
+    k.extra_height_top = 4;
+    k.extra_width_left = extra_width;
+  });
 
   Key b5 = bottom_template;
-  b5.parent = &m5;
-  b5.extra_z = 2;
-  b5.extra_height_bottom = 4;
-  b5.extra_width_left = 2;
-  keys.push_back(&b5);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &m5;
+    k.extra_z = 2;
+    k.extra_height_bottom = 4;
+    k.extra_width_left = extra_width;
+  });
 
   // Thumb
 
   // Top Right
   Key th1{22.5, -54.1, 50.2};
-  th1.t().rx = 13.3;
-  th1.t().ry = 21.6;
-  th1.t().rz = -9;
-  th1.t().y -= 2;
-  th1.AddTransform();
-  th1.t().z = 1;
-  th1.extra_height_top = 2;
-  th1.extra_height_bottom = 4;
-  th1.extra_width_right = 1;
-  th1.extra_z = 1;
-  keys.push_back(&th1);
+  configure_key(&t5, [&](Key& k) {
+    k.t().rx = 13.3;
+    k.t().ry = 21.6;
+    k.t().rz = -9;
+    k.t().y -= 2;
+    k.AddTransform();
+    k.t().z = 1;
+    k.extra_height_top = 2;
+    k.extra_height_bottom = 4;
+    k.extra_width_right = 1;
+    k.extra_z = 1;
+  });
 
   Key th2{20, -5, 3.2};
-  th2.parent = &th1;
-  th2.extra_z = 1;
-  th2.extra_height_bottom = 3;
-  keys.push_back(&th2);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &th1;
+    k.extra_z = 1;
+    k.extra_height_bottom = 3;
+
+    /*
+    k.t().ry = -10;
+    k.AddTransform();
+    k.t().x -= 1;
+    k.t().z = 2;
+    */
+  });
 
   Key th2_t{0, 20, 2.2};
-  th2_t.parent = &th2;
-  th2_t.extra_z = 2;
-  keys.push_back(&th2_t);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &th2;
+    k.extra_z = 2;
+
+    /*
+    k.t().rx = 5;
+    k.AddTransform();
+    k.t().y = -1;
+    k.t().z = 2;
+    */
+  });
 
   Key th3{20, -8, 1.5};
-  th3.parent = &th2;
-  th3.extra_z = 2;
-  th3.extra_height_bottom = 2;
-  th3.extra_width_right = 2;
-  keys.push_back(&th3);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &th2;
+    k.extra_z = 2;
+    k.extra_height_bottom = 2;
+    k.extra_width_right = extra_width;
+
+    /*
+    k.t().ry = -10;
+    k.AddTransform();
+    k.t().x -= 1;
+    k.t().z = 2;
+    */
+  });
 
   Key th3_t{0, 20, 4.2};
-  th3_t.parent = &th3;
-  th3_t.extra_z = 3.5;
-  th3_t.extra_height_top = 4;
-  th3_t.extra_width_right = 2;
-  keys.push_back(&th3_t);
+  configure_key(&t5, [&](Key& k) {
+    k.parent = &th3;
+    k.extra_z = 3.5;
+    k.extra_height_top = 4;
+    k.extra_width_right = extra_width;
+  });
 
   std::vector<Shape> shapes;
   for (Key* key : keys) {
@@ -364,10 +424,34 @@ int main() {
         Tri(th1.GetBottomLeft(), bb2.GetBottomRight(), th1.GetTopLeft(), Connector()),
         Tri(bb3.GetBottomRight(), bb2.GetBottomLeft(), bb2.GetBottomRight(), Connector()),
         Tri(bb3.GetBottomRight(), th1.GetBottomLeft(), bb2.GetBottomRight(), Connector()),
+        Tri(th2_t.GetTopRight(), b0.GetBottomRight(), b0.GetTopRight(), Connector()),
+        Tri(th2_t.GetTopRight(), th3_t.GetTopLeft(), th3_t.GetTopRight(), Connector()),
+        // Tri(th2_t.GetTopRight(), th3_t.GetTopRight(), b0.GetTopRight(), Connector()),
 
     };
 
     shapes.insert(shapes.end(), connectors.begin(), connectors.end());
+
+    std::vector<TransformList> wall_points = {
+        tt0.GetTopRight(),   tt0.GetTopLeft(),     tt1.GetTopRight(),   tt1.GetTopLeft(),
+        tt2.GetTopRight(),   tt2.GetTopLeft(),     tt3.GetTopRight(),   tt3.GetTopLeft(),
+        tt4.GetTopRight(),   tt4.GetTopLeft(),     tt5.GetTopRight(),   tt5.GetTopLeft(),
+        tt5.GetBottomLeft(), t5.GetTopLeft(),      t5.GetBottomLeft(),  m5.GetTopLeft(),
+        m5.GetBottomLeft(),  b5.GetTopLeft(),      b5.GetBottomLeft(),  b5.GetBottomRight(),
+        b4.GetBottomLeft(),  b4.GetBottomRight(),  bb3.GetBottomLeft(), bb3.GetBottomRight(),
+        th1.GetBottomLeft(), th1.GetBottomRight(), th2.GetBottomLeft(), th2.GetBottomRight(),
+        th3.GetBottomLeft(), th3.GetBottomRight(), th3.GetTopRight(),   th3_t.GetBottomRight(),
+        th3_t.GetTopRight(), th2_t.GetTopRight(),  b0.GetTopRight(),    m0.GetBottomRight(),
+        m0.GetTopRight(),    t0.GetBottomRight(),  t0.GetTopRight(),    tt0.GetBottomRight(),
+    };
+    auto get_post = [](TransformList t) {
+      Shape s = t.Apply(Connector());
+      return Hull(s, s.Projection().LinearExtrude(1));
+    };
+    for (size_t i = 0; i < wall_points.size(); ++i) {
+      shapes.push_back(
+          Hull(get_post(wall_points[i]), get_post(wall_points[(i + 1) % wall_points.size()])));
+    }
   }
 
   if (kShowManuform) {

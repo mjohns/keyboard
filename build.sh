377d5aee20f1afed3e925e3d366b07c004d0c063
Michael Johns <1633607+mjohns@users.noreply.github.com>
2020-04-12 11:48:06 -0400

    upd

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+	echo "Must specify target to build. ex: ergodox_extension"
+  exit 1
+fi
+
+if [ ! -f src/$1.cc ]; then
+	echo "src/$1.cc does not exist"
+fi
+
+if [ ! -d out ]; then
+	mkdir out
+fi
+cd out
+
+if [ ! -f scad.o ]; then
+	echo "Building util"
+	g++ -c -std=c++17 ../src/util/*.cc -I../src/util -I../src/glm/v0.9.9.2
+	if [ $? -ne 0 ]; then
+		echo "Failed to build util"
+		exit 1
+	fi
+fi
+
+echo "Building $1"
+g++ -std=c++17 ../src/$1.cc *.o -I../src/util -I../src/glm/v0.9.9.2 -o $1 && ./$1 && echo "Done"
